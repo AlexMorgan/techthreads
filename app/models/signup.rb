034056028d2e13cedbb45f0c7e0e2bfc49535f49
AlexMorgan <alexmorgan.am@gmail.com>
2015-04-06 22:54:49 -0400

    Added user agent and ip address to signup registration

--- app/models/signup.rb
+++ app/models/signup.rb
@@ -1,9 +1,10 @@
 class Signup < ActiveRecord::Base
   validates :email, :email => true,  uniqueness: true
+  # before_save :acquire_request_params
 
   def acquire_request_params(request)
-    user_agent = request.env["HTTP_USER_AGENT"]
-    remote_ip = request.remote_ip
-    binding.pry
+    self.user_agent = request.env["HTTP_USER_AGENT"]
+    self.ip_address = request.remote_ip
+    self.save
   end
 end
