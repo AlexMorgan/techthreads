b98c05e4dc4fc7d418564bd8bf7aec6a0313416a
AlexMorgan <alexmorgan.am@gmail.com>
2015-04-28 22:28:08 -0400

    Added twitter button functionality

--- app/assets/javascripts/referDialogue.js
+++ app/assets/javascripts/referDialogue.js
@@ -2,5 +2,7 @@ function formatReferDialogue(path) {
   $('#signup-content').children().fadeOut(400, function(){
     $('#referral-content').fadeIn(400);
     $('#referral-id-share').val(path);
+    var twHref = "https://twitter.com/share?text=Check out TechThreads. A startup looking to reinvent the way we wear tech&url=" + path;
+    $('.tw-share').attr('href', twHref)
   });
 }
