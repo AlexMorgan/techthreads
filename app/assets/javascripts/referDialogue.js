4a769843a05940d7e01a7a0424f570d649ea6256
AlexMorgan <alexmorgan.am@gmail.com>
2015-04-28 22:06:12 -0400

    Added facebook and twitter share icons

--- app/assets/javascripts/referDialogue.js
+++ app/assets/javascripts/referDialogue.js
@@ -1,6 +1,6 @@
 function formatReferDialogue(path) {
-  $('#splash-signup').children().hide();
-  $('#splash-signup').append('<h1>Thanks for believing in our mission!
-    <br>Share the following link to get exclusive deals:</h1>');
-  $('#splash-signup').append('<h4>' + path + '</h4>');
+  $('#signup-content').children().fadeOut(400, function(){
+    $('#referral-content').fadeIn(400);
+    $('#referral-id-share').val(path);
+  });
 }
